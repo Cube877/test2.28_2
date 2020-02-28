6dbde03186314df1915ad4a205d62121277f30e8
Cube <877211593@qq.com>
2020-02-28 15:21:38 +0800

    无

--- test2.28_2/test2.28_2.cpp
+++ test2.28_2/test2.28_2.cpp
@@ -1,11 +1,19 @@
 // test2.28_2.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
 
+#include "stdafx.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
 
 int main()
 {
-    return 0;
+	cv::Mat srcMat = imread("E:\\Picture\\Camera Roll\\1.jpg", 0);
+
+	imshow("src", srcMat);
+	waitKey(0);
+	return 0;
 }
 
